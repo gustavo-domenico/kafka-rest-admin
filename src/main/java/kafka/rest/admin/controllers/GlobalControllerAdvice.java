a7f029c59ff486dc0ef596868e6cb8f6d32af15b
Gustavo Di Domenico <gustavo.domenico@transferwise.com>
2020-01-19 16:43:08 +0200

    Test kafka configuration and connection.

---
+++ src/main/java/kafka/rest/admin/controllers/GlobalControllerAdvice.java
@@ -0,0 +1,24 @@
+package kafka.rest.admin.controllers;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalControllerAdvice {
+	@ModelAttribute
+	public void inject(@RequestParam("bootstrap-servers") String bootstrapServers, Model model) {
+		Map<String, Object> props = new HashMap<>();
+		props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+		props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+		props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+		model.addAttribute("consumerFactory", new DefaultKafkaConsumerFactory<>(props));
+	}
+}
