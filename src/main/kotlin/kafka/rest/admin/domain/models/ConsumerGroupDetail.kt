1528d41332b9039dad8ae053f6876ee53bb9f206
Gustavo Di Domenico <gustavo.domenico@transferwise.com>
2020-01-25 16:46:56 +0000

    Remove debug code.

--- src/main/kotlin/kafka/rest/admin/domain/models/ConsumerGroupDetail.kt
+++ src/main/kotlin/kafka/rest/admin/domain/models/ConsumerGroupDetail.kt
@@ -5,7 +5,6 @@ import org.apache.kafka.clients.admin.ConsumerGroupDescription
 data class ConsumerGroupDetail(val consumerGroup: ConsumerGroup, val state: String) {
     companion object {
         fun consumerGroupDetailOf(consumerGroupDescription: ConsumerGroupDescription): ConsumerGroupDetail {
-            println(consumerGroupDescription.toString())
             return ConsumerGroupDetail(
                     ConsumerGroup(consumerGroupDescription.groupId()),
                     consumerGroupDescription.state().toString())
