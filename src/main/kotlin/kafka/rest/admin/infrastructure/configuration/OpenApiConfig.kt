cfcb48514041bbf698c8d5605f30c8eb2e1cc68f
Gustavo Di Domenico <gustavo.domenico@transferwise.com>
2020-02-10 18:21:48 +0000

    Convert to kotlin class.

---
+++ src/main/kotlin/kafka/rest/admin/infrastructure/configuration/OpenApiConfig.kt
@@ -0,0 +1,18 @@
+package kafka.rest.admin.infrastructure.configuration
+
+import io.swagger.v3.oas.models.Components
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Info
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class OpenApiConfig {
+    @Bean
+    fun customOpenAPI(): OpenAPI {
+        return OpenAPI()
+                .components(Components())
+                .info(Info().title("Kafka Rest Admin").description(
+                        "Simple restful API to manage local kafka clusters."))
+    }
+}
