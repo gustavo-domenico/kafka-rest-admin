a7825fb4940dc20bdf9ae6b48e452c6d4978dff6
Gustavo Di Domenico <gustavo.domenico@transferwise.com>
2020-02-12 13:52:10 +0000

    Keep the same error handler for now.

--- src/main/kotlin/kafka/rest/admin/infrastructure/configuration/ErrorAttributesConfiguration.kt
+++ src/main/kotlin/kafka/rest/admin/infrastructure/configuration/ErrorAttributesConfiguration.kt
@@ -3,13 +3,10 @@ package kafka.rest.admin.infrastructure.configuration
 import org.springframework.boot.web.servlet.error.DefaultErrorAttributes
 import org.springframework.stereotype.Component
 import org.springframework.web.context.request.WebRequest
-import java.util.*
 
 @Component
 class ErrorAttributesConfiguration : DefaultErrorAttributes() {
     override fun getErrorAttributes(webRequest: WebRequest, includeStackTrace: Boolean): Map<String, Any> {
-        val errorAttributes: MutableMap<String, Any> = LinkedHashMap()
-        errorAttributes["timestamp"] = Date()
-        return errorAttributes
+        return super.getErrorAttributes(webRequest, includeStackTrace);
     }
 }
